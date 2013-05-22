c750978a1cf87c1633bb0255f66ce69692484f27
Daniel Lindblom <daniel_lindblom@mail.com>
2013-05-22 10:41:50 +0200

    Nya projektet

---
+++ Geekshirts/Geekshirts/GeekshirtsScreens/Make_your_own_t_shirt.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GeekshirtsScreens
+{
+	public partial class Make_your_own_t_shirt : UserControl
+	{
+		public Make_your_own_t_shirt()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
---
+++ Geekshirts/Geekshirts/GeekshirtsScreens/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Geekshirts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Geekshirts")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e565f055-2779-41eb-b79f-16c6b59a923c")]
