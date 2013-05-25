42cf9bfc9c842f475a4cc5b96f0019254dcf021f
Johan <skalman.se@gmail.com>
2013-05-25 02:13:13 +0200

    Startsida. Fungerade inte först. Gjort om

---
+++ Geekshirts/Geekshirts/GeekshirtsScreens/Copy_of_Screen_1_2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GeekshirtsScreens
+{
+	public partial class Copy_of_Screen_1_2 : UserControl
+	{
+		public Copy_of_Screen_1_2()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+		private void Image_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
