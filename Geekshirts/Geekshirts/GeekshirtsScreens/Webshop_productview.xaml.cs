4a73aa84e3ac4de14a0e8d6a17334abe8ffee0c8
Felix Tilli <felixtilli1@gmail.com>
2013-05-27 01:24:53 +0200

    + About, cart, webshop start, product view

---
+++ Geekshirts/Geekshirts/GeekshirtsScreens/Webshop_productview.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GeekshirtsScreens
+{
+	public partial class Webshop_productview : UserControl
+	{
+		public Webshop_productview()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
